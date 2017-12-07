79bbad162fdb661ececc069b526d43afc95e1914
Mat Ryer <thatmatryer@gmail.com>
2017-12-07 15:39:43 +0000

    tweaked format

--- doc.go
+++ doc.go
@@ -1,19 +1,19 @@
 // Package graphql provides a low level GraphQL client.
 //
-//     ctx := context.Background()
-//     ctx = graphql.NewContext(ctx, "https://machinebox.io/graphql")
-//     r := graphql.NewRequest(`
-//         query ($key: String!) {
-//	           items (id:$key) {
-//	               field1
-//                 field2
-//                 field3
-//             }
+// ctx := context.Background()
+// ctx = graphql.NewContext(ctx, "https://machinebox.io/graphql")
+// r := graphql.NewRequest(`
+//     query ($key: String!) {
+//         items (id:$key) {
+//             field1
+//             field2
+//             field3
 //         }
-//     `)
-//     r.Var("key", "value")
-//     var respData ResponseStruct
-//     if err := r.Run(ctx, &respData); err != nil {
-//         log.Fatalln(err)
 //     }
+// `)
+// r.Var("key", "value")
+// var respData ResponseStruct
+// if err := r.Run(ctx, &respData); err != nil {
+//     log.Fatalln(err)
+// }
 package graphql
